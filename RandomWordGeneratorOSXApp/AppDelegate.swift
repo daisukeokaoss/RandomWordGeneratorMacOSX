1ec0a8d7fcf97c92520ec49804d842f3d28e4d51
岡大輔 <daisukeokahassou@gmail.com>
2016-01-02 02:07:51 +0900

    Initial Commit

---
+++ RandomWordGeneratorOSXApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomWordGeneratorOSXApp
+//
+//  Created by 岡大輔 on 2016/01/02.
+//  Copyright © 2016年 岡大輔. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomWordGeneratorOSXApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomWordGeneratorOSXApp
+//
+//  Created by 岡大輔 on 2016/01/02.
+//  Copyright © 2016年 岡大輔. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
