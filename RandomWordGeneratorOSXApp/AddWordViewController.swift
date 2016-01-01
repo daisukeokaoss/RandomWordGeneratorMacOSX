3f578f9bb19d02afb0e0f168b31400bdd7f0a9c7
岡大輔 <daisukeokahassou@gmail.com>
2016-01-02 03:02:07 +0900

    ダイアログボックス実装

--- RandomWordGeneratorOSXApp/AddWordViewController.swift
+++ RandomWordGeneratorOSXApp/AddWordViewController.swift
@@ -8,9 +8,29 @@
 
 import Cocoa
 
+
+
 class AddWordViewController: NSViewController {
 
     @IBAction func AddWordClick(sender: NSButton) {
         sender.title = "キャンセル"
+        func dialogOKCancel(question: String, text: String) -> Bool {
+            let myPopup: NSAlert = NSAlert()
+            myPopup.messageText = question
+            myPopup.informativeText = text
+            myPopup.alertStyle = NSAlertStyle.WarningAlertStyle
+
+            myPopup.accessoryView = NSTextField(frame: NSRect(x:0,y: 0,width:  200,height:  24))
+            myPopup.addButtonWithTitle("OK")
+            myPopup.addButtonWithTitle("Cancel")
+            let res = myPopup.runModal()
+            if res == NSAlertFirstButtonReturn {
+                return true
+            }
+            return false
+        }
+        
+        let answer = dialogOKCancel("Ok?", text: "Choose your answer.")
+        //Debug.WriteLine(name);
     }
 }
